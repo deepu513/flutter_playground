57b103a74bf26325eb20a4508ef5590b714485a8
Deepak <dpkramrakhyani@gmail.com>
2019-08-26 19:20:54 +0530

    Started implementing navigation

--- lib/src/news/app.dart
+++ lib/src/news/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/src/news/src/blocs/stories_provider.dart';
+import 'package:flutter_playground/src/news/src/screens/news_details.dart';
 import 'package:flutter_playground/src/news/src/screens/news_list_screen.dart';
 
 class NewsApp extends StatelessWidget {
@@ -9,8 +10,20 @@ class NewsApp extends StatelessWidget {
       child: MaterialApp(
         title: 'News',
         home: NewsListScreen(),
+        onGenerateRoute: routes,
       ),
     );
   }
 
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(builder: (context) {
+        return NewsListScreen();
+      });
+    } else {
+      return MaterialPageRoute(builder: (context) {
+        return NewsDetails();
+      });
+    }
+  }
 }
