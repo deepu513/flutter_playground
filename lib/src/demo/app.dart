8daee90f82e03ce1b8d6b35515701b72f3d5f6f1
Deepak <deepak.ramrakhyani@celusion.com>
2020-03-17 14:01:27 +0530

    Added status bar color

--- lib/src/demo/app.dart
+++ lib/src/demo/app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_playground/src/demo/screens/HomeScreen.dart';
+import 'package:flutter/services.dart';
 
 class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var greenColor = Color.fromARGB(255, 51, 175, 133);
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark
+    ));
 
     return MaterialApp(
       home: SafeArea(
--- lib/src/demo/screens/HomeScreen.dart
+++ lib/src/demo/screens/HomeScreen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 
 class HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
+  final List<Widget> _children = [FirstTabContent(), SecondTabContent(), FirstTabContent(), SecondTabContent()];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -18,31 +19,51 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: true,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            title: Text('Subject'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.nature),
-            title: Text('Growing'),
-          ), BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('My'),
-          ),
-        ],
-        unselectedItemColor: Colors.grey,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+      body: _children[_selectedIndex],
+      backgroundColor: Colors.white,
+      bottomNavigationBar: _getBottomNavBar(),
     );
   }
 
+  _getBottomNavBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      showUnselectedLabels: true,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          title: Text('Subject'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.nature),
+          title: Text('Growing'),
+        ), BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          title: Text('My'),
+        ),
+      ],
+      unselectedItemColor: Colors.grey,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+
+}
+
+class FirstTabContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SecondTabContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
